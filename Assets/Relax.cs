7d472cad470f04cbc19c68ad418ebc695954e733
Аристарх <127043429+Arik7u7@users.noreply.github.com>
2023-12-24 15:50:42 +0300

    stupid guild

--- Assets/Relax.cs
+++ Assets/Relax.cs
@@ -4,17 +4,4 @@ using UnityEngine;
 
 public class Relax : MonoBehaviour
 {
-    [SerializeField] private MeshRenderer _renderer;
-    private Relaxation _playerRelax;
-
-    private void OnEnable()
-    {
-        _playerRelax = Player.Instance.GetComponent<Relaxation>();
-        _playerRelax.Changed += ChangeConcetrationBarValue;
-    }
-
-    private void ChangeConcetrationBarValue()
-    {
-        _renderer.material.SetFloat("_Health", _playerRelax.Value);
-    }
 }
